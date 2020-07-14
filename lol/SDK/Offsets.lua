5e456f40ddf2611faf0b18aebc4c5deeb8bfe611
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2020-07-14 10:40:04 +0200

    Update Offsets.lua

--- lol/SDK/Offsets.lua
+++ lol/SDK/Offsets.lua
@@ -6,8 +6,8 @@ local Offsets = {}
 
 local modulePtr = ptrtonumber(winapi.getModuleHandle(0))
 
-Offsets.CastSpell = modulePtr + 0x5177E0
-Offsets.CastSock = modulePtr + 0x350E10D
+Offsets.CastSpell = modulePtr + 0x517C00
+Offsets.CastSock = modulePtr + 0x1bfb670
 Offsets.GetAttackCastDelay = modulePtr + 0x2b62b0
 Offsets.GetAttackDelay = modulePtr + 0x2b63b0
 Offsets.PrintChat = modulePtr + 0x57a9d0
